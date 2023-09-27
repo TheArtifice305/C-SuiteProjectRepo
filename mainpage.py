f34f7cf30f843ccc2cdf540208535798ae91b168
Alex Welch <welcha7@xavier.edu>
2023-09-27 12:14:48 -0400

    Updates

--- mainpage.py
+++ mainpage.py
@@ -1,17 +1,11 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
 mainpage = Flask(__name__)
 db = SQLAlchemy()
 mainpage.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 mainpage.config['SECRET_KEY'] = 'accessplease'
 db.init_app(mainpage)
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
 
 
 @mainpage.route('/')
