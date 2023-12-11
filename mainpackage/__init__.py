7de3fde91df85408fb4a662bc4ac772ca18b4274
QBarbs <qcroscoe@icloud.com>
2023-12-10 22:27:04 -0500

    Added signups functionality along with some bugfixing and dashboard.html displays user's email.

--- mainpackage/__init__.py
+++ mainpackage/__init__.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, url_for, redirect, request, flash, g, session, Blueprint
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from mainpackage.authentication import login_required
 from werkzeug.security import generate_password_hash
-from datetime import datetime
-from pytz import timezone
-#app = Flask(__name__)
+
+
 app = Flask(__name__, instance_relative_config=True)
 app.config['SECRET_KEY'] = 'accessplease'
 
@@ -19,29 +18,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 db.init_app(app)
 
 
+student_posts = db.Table('student_posts', db.Column('users_id', db.Integer, db.ForeignKey('users.user_id')),
+                         db.Column('posters_id', db.Integer, db.ForeignKey('posters.id')))
+
+community_partner_posts = db.Table('community_partner_posts', db.Column('users_id', db.Integer,
+                                                                        db.ForeignKey('users.user_id')),
+                                   db.Column('posters_id', db.Integer, db.ForeignKey('posters.id')))
+
+
 class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.Text)
     role = db.Column(db.String)
-
-
-class Opportunity(db.Model):
-    __tablename__ = 'opportunities'
-    opportunity_id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(99999), nullable=False)
-    datetime = db.Column(db.DateTime)
-    occurance = db.Column(db.String(100))
+    joined_posts = db.relationship('Poster', secondary=student_posts, backref='student_posts')
+    created_posts = db.relationship('Poster', secondary=community_partner_posts, backref='community_partner_posts')
 
 
 class Poster(db.Model):
+    __tablename__ = 'posters'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False, unique=True)
+    title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    posted_by = db.Column(db.String(20), nullable=False, default='N/A')
+    posted_by = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     posted_on = db.Column(db.DateTime, nullable=False)
     occurance_from = db.Column(db.Text)
 
